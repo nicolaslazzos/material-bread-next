8fdde47843bde4acd300cd8726e0c1f10cb0a148
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-10-27 21:10:22 -0300

    added react-native and updated next plugins and next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,28 @@
-const withTM = require("next-transpile-modules");
 const withFonts = require("next-fonts");
+const withTM = require("next-transpile-modules")([
+  "material-bread",
+  "react-native-svg",
+  "react-native-vector-icons",
+  "@react-native-community/toolbar-android",
+  "react-native",
+]);
 
-module.exports = withFonts(
-  withTM({
-    transpileModules: [
-      "material-bread",
-      "react-native-svg",
-      "react-native-vector-icons",
-      "@react-native-community/toolbar-android"
-    ],
-    webpack: (config) => {
-      config.resolve.extensions = [".web.js", ".js"];
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        "react-native$": "react-native-web",
-        "react-native-svg$": "react-native-svg-web",
-        'react-native/Libraries/Components/UnimplementedViews/UnimplementedView$': 'react-native-web/dist/modules/UnimplementedView'
-      };
-      return config;
-    },
-  })
-);
+const withPlugins = require("next-compose-plugins");
+
+const plugins = [withFonts, withTM];
+
+let nextOptions = {
+  webpack: (config) => {
+    config.resolve.extensions = [".web.js", ".js"];
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "react-native$": "react-native-web",
+      "react-native-svg$": "react-native-svg-web",
+      "react-native/Libraries/Components/UnimplementedViews/UnimplementedView$":
+        "react-native-web/dist/modules/UnimplementedView",
+    };
+    return config;
+  },
+};
+
+module.exports = withPlugins(plugins, nextOptions);
